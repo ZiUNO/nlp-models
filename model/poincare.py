6c0e8b655c5c3cfa8454e1539880eaa8c8d0d0c4
ZiUNO <331245059@qq.com>
2020-10-29 21:32:26 +0800

    Modify: poincare v0.1

--- model/poincare.py
+++ model/poincare.py
@@ -5,57 +5,61 @@
 # @File    : poincare.py
 # @Software: PyCharm
 import torch
-from torch.autograd import Function, Variable
+from torch.autograd import Function
 
 eps = 1e-6
 boundary = 1 - eps
 
 
-class Arcosh(Function):
-    @staticmethod
-    def forward(ctx, x):
-        z = torch.sqrt(x ** 2 - 1)
-        ctx.save_for_backward(z)
-        return torch.log(x + z)
-
-    @staticmethod
-    def backward(ctx, grad_outputs):
-        z, = ctx.saved_tensors
-        z = torch.clamp(z, min=eps)
-        grad = grad_outputs / z
-        return grad
+# class Arcosh(Function):
+#     @staticmethod
+#     def forward(ctx, x):
+#         z = torch.sqrt(x ** 2 - 1)
+#         ctx.save_for_backward(z)
+#         return torch.log(x + z)
+#
+#     @staticmethod
+#     def backward(ctx, grad_outputs):
+#         z, = ctx.saved_tensors
+#         z = torch.clamp(z, min=eps)
+#         grad = grad_outputs / z
+#         return grad
 
 
 class PoincareDistance(Function):
     @staticmethod
+    def norm_square(x):
+        return torch.clamp(torch.norm(x, p=2, dim=-1), min=0, max=boundary) ** 2
+
+    @staticmethod
     def forward(ctx, x, y):
-        x_norm = torch.clamp(torch.norm(x, p=2, dim=-1), min=0, max=boundary)
-        y_norm = torch.clamp(torch.norm(y, p=2, dim=-1), min=0, max=boundary)
-        result = 1 + 2 * torch.norm(x - y, p=2, dim=-1) ** 2 / ((1 - x_norm ** 2) * (1 - y_norm ** 2))
+        x_norm_square = PoincareDistance.norm_square(x)
+        y_norm_square = PoincareDistance.norm_square(y)
+        alpha = 1 + 2 * PoincareDistance.norm_square(x - y) / ((1 - x_norm_square) * (1 - y_norm_square))
 
         # arcosh
-        z = torch.sqrt(result ** 2 - 1)
-        ctx.save_for_backward(x, y, z)
-        return torch.log(result + z)
+        arc_grad = torch.sqrt(alpha ** 2 - 1)
+        ctx.save_for_backward(x, y, arc_grad)
+        return torch.log(alpha + arc_grad)
 
     @staticmethod
     def backward(ctx, grad_outputs):
-        x, y, z = ctx.saved_tensors
+        x, y, arc_grad = ctx.saved_tensors
 
         # arcosh'
-        z = torch.clamp(z, min=eps)
+        arc_grad = torch.clamp(arc_grad, min=eps)
 
         x_y = x - y
-        x_y_norm_2 = torch.norm(x_y, p=2, dim=-1) ** 2
+        x_y_norm_2 = PoincareDistance.norm_square(x_y)
         x_y_norm_2 = x_y_norm_2.view(x.shape[0], x.shape[1], 1).expand(x.shape)
         x_norm_2_y_norm_2 = (1 - torch.norm(x, p=2, dim=-1) ** 2) * (1 - torch.norm(y, p=2, dim=-1) ** 2)
         x_norm_2_y_norm_2 = x_norm_2_y_norm_2.view(x.shape[0], x.shape[1], 1).expand(x.shape)
         x_grad = 4 * x_y / x_norm_2_y_norm_2 - 4 * x * x_y_norm_2 / x_norm_2_y_norm_2 ** 2
         y_grad = -4 * x_y / x_norm_2_y_norm_2 - 4 * y * x_y_norm_2 / x_norm_2_y_norm_2 ** 2
 
-        grad_inputs = grad_outputs.detach().view(x.shape[0], x.shape[1], 1).expand(x.shape)
-        z = z.view(x.shape[0], x.shape[1], 1).expand(x.shape)
-        return grad_inputs * z * x_grad, grad_inputs * z * y_grad
+        grad_inputs = grad_outputs.detach().view(x.shape[0], x.shape[1], 1).expand_as(x)
+        arc_grad = arc_grad.view(x.shape[0], x.shape[1], 1).expand_as(x)
+        return grad_inputs * arc_grad * x_grad, grad_inputs * arc_grad * y_grad
 
 
 def poincare_distance(x, y):
