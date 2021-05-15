f15c9b3943d617241ff54798b57d5d50504a7cd9
ZiUNO <331245059@qq.com>
2021-05-15 14:14:46 +0800

    Add __init__.py in dir so that can use command line: allennlp train -s allennlp_model/glove_model --include-package allennlp_model -f allennlp_model/run_glove_embedder.json directly include-package dir to use model-name in config.json

---
+++ allennlp_model/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/15 2:10 下午
+# @Author  : ZiUNO
+# @Email   : ziunocao@126.com
+# @File    : __init__.py
+# @Software: PyCharm
+
+from allennlp_model.embedder.glove_embedder import GloVeEmbedding
+
+__all__ = ["GloVeEmbedding"]
